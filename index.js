7df9492b42bda8fd0873ad5d082ec610b0742c79
Ollie Edwards <ollie@brandwatch.com>
2016-05-12 16:44:35 +0100

    Some error handling examples

--- index.js
+++ index.js
@@ -4,38 +4,70 @@ const test = require('tape'),
       http = require('http');
 
 const CONTENT = 'content';
+
 const defaultHandler = (req, res) => res.end(CONTENT);
-let handler1 = defaultHandler;
-let handler2 = defaultHandler;
 
-const server = http.createServer((req, res) => handler1(req, res));
-server.listen(1111);
+function createServers(handler1, handler2) {
+    handler1 = handler1 || defaultHandler;
+    handler2 = handler2 || defaultHandler;
+    
+    const server1 = http.createServer((req, res) => handler1(req, res));
+    server1.listen(1111);
 
-const server2 = http.createServer((req, res) => handler2(req, res));
-server2.listen(2222);
-
+    const server2 = http.createServer((req, res) => handler2(req, res));
+    server2.listen(2222);
+    
+    return end => Promise.all([new Promise(res => server1.close(res)), new Promise(res => server2.close(res))]).then(() => end());
+}
 
 test('ping', t => {
-    handler1 = (req, res) => {
+    const close = createServers((req, res) => {
         res.statusCode = 678;
         res.end('pong');
-    };
+    });
     http.get('http://localhost:1111', res => {
         t.equals(res.statusCode, 678);
         res.on('data', d => {
             t.equals(d.toString('utf8'), 'pong'); 
-            t.end();
+            close(t.end);
         });
     });
 });
 
 test('proxy', t => {
-    handler1 = (req, res) => http.get('http://localhost:2222', pres => pres.pipe(res));
-    handler2 = defaultHandler;
+    const close = createServers((req, res) => http.get('http://localhost:2222', pres => pres.pipe(res)));
     http.get('http://localhost:1111', res => {
         res.on('data', d => {
             t.equals(d.toString('utf8'), CONTENT);
-            t.end();
+            close(t.end);
+        });
+    });
+});
+
+test('error handling a bogus url', t => {
+    const handler = (req, res) => http.get('http://foo.bar.lit', pres => pres.pipe(res)).on('error', e => {
+        res.writeHead(500);
+        res.end();
+    });
+    const close = createServers(handler);
+    http.get('http://localhost:1111', res => {
+        t.equals(res.statusCode, 500);
+        close(t.end);
+    });  
+});
+
+test('error in server2 req', t => {
+    const handler1 = (req, res) => http.get('http://localhost:2222', pres => pres.pipe(res)).on('error', e=> {
+        res.statusCode =500;
+        res.end(e.message);
+    }).emit('error', new Error('TROLOLOL'));
+    const handler2 = (req, res) => {};
+
+    const close = createServers(handler1, handler2);
+    http.get('http://localhost:1111', res => {
+        res.on('data', d => {
+            t.equals(d.toString('utf8'), 'TROLOLOL');
+            close(t.end);
         });
     });
 });
